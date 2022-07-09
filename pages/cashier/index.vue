8bae660efb0963f65472e1788446a60e8c8754d4
Yabodo <yabood@vivaldi.net>
2022-07-10 00:07:46 +0300

    Trial 10

---
+++ pages/cashier/index.vue
@@ -0,0 +1,476 @@
+<script setup lang="ts">
+const orderUrlOrigin = "https://pastaljano.surge.sh/order/"
+
+
+import {
+  range as _range,
+  random as _random,
+  get as _get,
+  pull as _pull,
+  findIndex as _findIndex,
+  concat as _concat,
+} from "lodash";
+
+import QRCode from 'qrcode'
+const route = useRoute()
+
+async function getQRCode(message: String) {
+  const finalURL = orderUrlOrigin.concat(message)
+  const response = await QRCode.toDataURL(finalURL)
+  .catch(err => {
+    console.error(err)
+  })
+  qrId.value = response;
+  return response
+}
+
+const qrId = ref('')
+
+
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+TimeAgo.addDefaultLocale(en)
+const timeAgo = new TimeAgo('en-US')
+
+console.log(timeAgo.format(new Date()))
+
+import type { RealtimeSubscription } from '@supabase/supabase-js'
+let subscription: RealtimeSubscription
+
+// Auth
+// profile attrs
+type ProfileAttrs = {
+  username?: string;
+  website?: string;
+  avatar_url?: string;
+};
+const allowedIds = ["7100edbc-1590-4244-b595-c2dac0f7f2cb"];
+const allowedEmails = [
+  "kjoeveer@gmail.com",
+  "yabodo@vivaldi.net",
+  "pastaljano@gmail.com",
+  "alexaus2002@gmail.com",
+  "Freddy.Jakobson@gmail.com",
+  "goodmenmedia@gmail.com",
+  "avainaru@gmail.com",
+  "aureliakene@gmail.com",
+];
+const user = useSupabaseUser();
+const client = useSupabaseClient();
+if (user.value) {
+  if (!allowedEmails.includes(user.value.email)) {
+    console.info(
+      "Nice try! We log your actions, but no harm done ;) If you should have access or want to work together, feel free to contact me in LinkedIn - Kaspar Jõeveer"
+    );
+    await signOut();
+  }
+} else signOut();
+
+async function signOut() {
+  const { error } = await client.auth.signOut();
+  navigateTo("/");
+}
+
+// Meta
+useHead({
+  bodyAttrs: {
+    class: "bg-secondary",
+  },
+});
+
+// Get Data
+const { data: ordersListData, refresh: refreshOrder } = await useAsyncData("orders", async () => {
+  const { data } = await client
+    .from("orders")
+    .select("id, name, created_at, done_by, source")
+    .order("done_by", { ascending: true, nullsFirst: true })
+    .order("created_at", { ascending: true })
+  .is('done_by', null);
+
+  return data;
+});
+
+const orderList = reactive(ordersListData);
+console.log(orderList);
+
+const { data: menuData } = await useAsyncData("menu", async () => {
+  const { data } = await client
+    .from("menu")
+    .select("id, type, name, shortname, description, price, vegan, alcohol")
+    .order("shortname", { ascending: true, nullsFirst: false });
+
+  return data;
+});
+menuData._rawValue.forEach(function(element) {
+  element.quantity = 0;
+});
+
+async function createOrderItems(orderId: String) {
+  let payload = [];
+  order.all.forEach((item) => {
+    console.log(item);
+    payload.push({
+      order_id: orderId,
+      menu_id: item,
+    });
+  });
+  const response = await client.from("order_items").insert(payload);
+  if (response.error) return response.error;
+  return response;
+}
+
+async function createOrder() {
+  const { data, error } = await client
+    .from("orders")
+    .insert([{ source: "website", name: clientName.value }]);
+  if (error) return error;
+  return { id: data[0].id, error };
+}
+
+async function submitOrder() {
+  let onlyDrinks = (this.order.pasta.length+this.order.pizza.length) == 0
+  if(onlyDrinks) clientName.value = "Drinker"
+  if (
+    this.order?.all?.length > 0 &&
+    clientName.value != "" &&
+    !buttonsLoading.value
+  ) {
+    buttonsLoading.value = true;
+    const response = await createOrder();
+    if (response.error) return response.error;
+    else {
+      console.log(response.id);
+      const items = await createOrderItems(response.id);
+      clientName.value = "";
+      if(onlyDrinks) orderDelivered(response.id)
+      else getQRCode(response.id);
+
+      removeAllItems();
+      //navigateTo(`/order/${response.id}`);
+      buttonsLoading.value = false;
+    }
+  }
+}
+
+async function orderDelivered(id: String) {
+  const { data, error } = await client
+    .from("orders")
+    .update({ done_by: ((new Date()).toISOString()) })
+    .eq('id', id)
+  if (error) return error;
+  return { id: data[0].id, error };
+}
+
+const menu = reactive(menuData);
+
+const clientName = ref("");
+const buttonsLoading = ref(false);
+
+const order = reactive({
+  pasta: [],
+  pizza: [],
+  lemonade: [],
+  beer: [],
+  wine: [],
+  all: [],
+});
+
+// Methods
+function addItem(type: String, itemId: number) {
+  this.menu[itemId].quantity++;
+
+  _get(this.order, [type]).push(this.menu[itemId].id);
+
+  this.order.all.push(this.menu[itemId].id);
+}
+
+function removeItem(type: String, itemId: number) {
+  if (this.menu[itemId].quantity > 0)
+    this.menu[itemId].quantity = this.menu[itemId].quantity - 1;
+
+  let typeIndex = _get(this.order, [type]).indexOf(this.menu[itemId].id);
+  if (typeIndex > -1) {
+    _get(this.order, [type]).splice(typeIndex, 1);
+  }
+
+  let allIndex = this.order.all.indexOf(this.menu[itemId].id);
+  if (allIndex > -1) {
+    this.order.all.splice(allIndex, 1);
+  }
+}
+
+function removeAllItems() {
+  order.pasta.length = 0;
+  order.pizza.length = 0;
+  order.lemonade.length = 0;
+  order.wine.length = 0;
+  order.beer.length = 0;
+  order.all.length = 0;
+  menu.value.forEach(function(item) {
+    item.quantity = 0;
+  });
+}
+
+// Computed
+const totalPrice = computed(() => {
+  let price = 0;
+  menu.value.forEach((item) => {
+    price = price + item.price * item.quantity;
+  });
+  return price;
+});
+
+// Mounted
+onMounted(() => {
+  subscription = client.from('orders').on('*', () => {
+    refreshOrder()
+  }).subscribe()
+})
+onUnmounted(() => {
+  client.removeSubscription(subscription)
+})
+</script>
+
+<template>
+  <section class="text-grey-700 body-font">
+    <!-- Dashboard -->
+    <div
+      class="container px-5 py-6 mx-auto flex-col justify-center items-center w-fit"
+    >
+      <div v-if="qrId != ''">
+        <img class="mx-auto mb-4" @click="qrId = ''" :src="qrId" alt="" />
+      </div>
+      <div class="flex">
+        <div class="w-fit">
+          <div
+            class="w-48 text-grey-900 bg-white border border-grey-200 rounded-lg mb-2"
+          >
+            <button
+              aria-current="true"
+              type="button"
+              class="w-full px-4 py-2 font-medium text-left text-white bg-blue-700 border-b border-grey-200 rounded-t-lg cursor-pointer focus:outline-none"
+            >
+              Pasta
+            </button>
+            <div class="relative" v-for="(item, i) in menu" :key="i">
+              <div v-if="item.type == 'pasta'">
+                <button
+                  @click="addItem(item?.type, i)"
+                  type="button"
+                  class="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b text-grey-900 border-grey-200 rounded-t-lg hover:bg-grey-100 hover:text-blue-700 focus:text-blue-700"
+                >
+                  <div v-if="item.quantity > 0" class="mr-2">
+                    <b>{{ item.quantity }}x</b>
+                  </div>
+                  {{ item.shortname }}
+                </button>
+                <button
+                  v-if="item.quantity > 0"
+                  @click="removeItem(item.type, i)"
+                  class="pl-2 py-2 absolute top-1/2 -translate-y-1/2 right-2 text-blue-600"
+                >
+                  <svg
+                    class="w-5 h-5"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            class="w-48 text-grey-900 bg-white border border-grey-200 rounded-lg mb-2"
+          >
+            <button
+              aria-current="true"
+              type="button"
+              class="w-full px-4 py-2 font-medium text-left text-white bg-blue-700 border-b border-grey-200 rounded-t-lg cursor-pointer focus:outline-none"
+            >
+              Pitsa
+            </button>
+            <div class="relative" v-for="(item, i) in menu" :key="i">
+              <div v-if="item.type == 'pizza'">
+                <button
+                  @click="addItem(item?.type, i)"
+                  type="button"
+                  class="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b text-grey-900 border-grey-200 rounded-t-lg hover:bg-grey-100 hover:text-blue-700 focus:text-blue-700"
+                >
+                  <div v-if="item.quantity > 0" class="mr-2">
+                    <b>{{ item.quantity }}x</b>
+                  </div>
+                  {{ item.shortname }}
+                </button>
+                <button
+                  v-if="item.quantity > 0"
+                  @click="removeItem(item.type, i)"
+                  class="pl-2 py-2 absolute top-1/2 -translate-y-1/2 right-2 text-blue-600"
+                >
+                  <svg
+                    class="w-5 h-5"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            class="w-48 text-grey-900 bg-white border border-grey-200 rounded-lg"
+          >
+            <button
+              aria-current="true"
+              type="button"
+              class="w-full px-4 py-2 font-medium text-left text-white bg-blue-700 border-b border-grey-200 rounded-t-lg cursor-pointer focus:outline-none"
+            >
+              Joogid
+            </button>
+            <div class="relative" v-for="(item, i) in menu" :key="i">
+              <div
+                v-if="
+                  item?.type != 'pizza' && item?.type != 'pasta' && item?.type
+                "
+              >
+                <button
+                  @click="addItem(item.type, i)"
+                  type="button"
+                  class="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b text-grey-900 border-grey-200 rounded-t-lg hover:bg-grey-100 hover:text-blue-700 focus:text-blue-700"
+                >
+                  <div v-if="item.quantity > 0" class="mr-2">
+                    <b>{{ item.quantity }}x</b>
+                  </div>
+                  {{ item.shortname }}
+                </button>
+                <button
+                  v-if="item.quantity > 0"
+                  @click="removeItem(item.type, i)"
+                  class="pl-2 py-2 absolute top-1/2 -translate-y-1/2 right-2 text-blue-600"
+                >
+                  <svg
+                    class="w-5 h-5"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <form autocomplete="off" @submit.prevent="submitOrder()">
+              <input
+                v-model="clientName"
+                type="text"
+                id="website-admin"
+                class="rounded bg-grey-50 border border-grey-300 text-grey-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5"
+                placeholder="Satoshi Nakamoto"
+              />
+            </form>
+          </div>
+          <div class="flex my-3 items-center justify-between w-full">
+            <buttonTrash @click="removeAllItems()" />
+            <div>
+              <b>€{{ totalPrice }}</b>
+            </div>
+            <buttonFire @click="submitOrder()" />
+          </div>
+        </div>
+        <div class="max-h-fit overflow-y-auto mx-2">
+          <div class="relative overflow-x-auto shadow-md">
+            <table
+              class="w-full text-sm text-left text-grey-500 bg-blue-700 border border-grey-200 rounded-xl"
+            >
+              <thead class="text-white">
+                <tr>
+                  <th scope="col" class="px-6 text-base font-medium p-2">
+                    Tellimused
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(item, i) in orderList"
+                  :key="i"
+                  class="bg-white border-b hover:bg-grey-50"
+                >
+                  <th
+                    scope="row"
+                    class="px-6 py-2 font-medium text-grey-900 whitespace-nowrap"
+                  >
+                    {{ item.name }} ({{
+                      timeAgo.format(Date.parse(item.created_at), "twitter")
+                    }})
+                  </th>
+                  <td
+                    @click="orderDelivered(item.id)"
+                    class="px-3 py-2 text-right cursor-pointer"
+                  >
+                    <div class="font-medium text-blue-600 hover:underline">
+                      <svg
+                        class="w-5 h-5"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </td>
+                  <td @click="getQRCode(item.id)" class="px-3 py-2 text-right">
+                    <div class="font-medium text-blue-600 hover:underline">
+                      <svg
+                        class="w-5 h-5"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ pages/order/[id].vue
@@ -0,0 +1,217 @@
+<script setup lang="ts">
+import type { RealtimeSubscription } from '@supabase/supabase-js'
+
+
+import {
+  range as _range,
+  random as _random,
+  get as _get,
+  pull as _pull,
+  findIndex as _findIndex,
+  concat as _concat,
+} from "lodash";
+
+const route = useRoute();
+
+// Auth
+// profile attrs
+type ProfileAttrs = {
+  username?: string;
+  website?: string;
+  avatar_url?: string;
+};
+const allowedIds = ["7100edbc-1590-4244-b595-c2dac0f7f2cb"];
+const allowedEmails = [
+  "kjoeveer@gmail.com",
+  "alexaus2002@gmail.com",
+  "Freddy.Jakobson@gmail.com",
+  "goodmenmedia@gmail.com",
+  "avainaru@gmail.com",
+  "aureliakene@gmail.com",
+];
+const user = useSupabaseUser();
+const client = useSupabaseClient();
+
+let subscription: RealtimeSubscription
+
+async function signOut() {
+  const { error } = await client.auth.signOut();
+  navigateTo("/login");
+}
+
+// Meta
+useHead({
+  bodyAttrs: {
+    class: "bg-secondary",
+  },
+});
+
+// Get Data
+const { data: menuData } = await useAsyncData("menu", async () => {
+  const { data } = await client
+    .from("menu")
+    .select("id, type, name, description, price, vegan, alcohol");
+
+  return data;
+});
+const { data: orderData, refresh: refreshOrder } = await useAsyncData("orders", async () => {
+  const { data } = await client
+    .from("orders")
+    .select("id, source, created_at, done_by")
+    .eq("id", route.params.id)
+    .single();
+  return data;
+});
+onMounted(() => {
+  subscription = client.from('orders').on('*', () => {
+    refreshOrder()
+  }).subscribe()
+})
+onUnmounted(() => {
+  client.removeSubscription(subscription)
+})
+
+if(orderData.value == null) {
+  navigateTo('/')
+}
+
+
+const { data: orderItemsData } = await useAsyncData("order_items", async () => {
+  const { data } = await client
+    .from("order_items")
+    .select("id, menu_id")
+    .eq("order_id", route.params.id);
+  return data;
+});
+const { data: profileData } = await useAsyncData("profiles", async () => {
+  if (user.value) {
+    const { data } = await client
+      .from("profiles")
+      .select("id, username, avatar_url, website, updated_at, admin")
+      .eq("id", user.value.id);
+    return data;
+  }
+  return false;
+});
+const menu = reactive(menuData);
+
+const order = reactive({
+  pasta: [],
+  pizza: [],
+  lemonade: [],
+  beer: [],
+  wine: [],
+  all: [],
+});
+
+// Methods
+
+function removeAllItems() {
+  order.pasta.length = 0;
+  order.pizza.length = 0;
+  order.lemonade.length = 0;
+  order.wine.length = 0;
+  order.beer.length = 0;
+  order.all.length = 0;
+  menu.value.forEach(function(item) {
+    item.quantity = 0;
+  });
+}
+
+function countArrayElements(arr) {
+  const count = {};
+  arr.forEach((element) => {
+    count[element] = (count[element] || 0) + 1;
+  });
+  return count;
+}
+
+let orderItems = {};
+let orderList = [];
+orderItemsData.value.forEach((element) => {
+  orderItems[element.menu_id] = (orderItems[element.menu_id] || 0) + 1;
+});
+for (const [key, value] of Object.entries(orderItems)) {
+  orderList.push({
+    item_id: key,
+    quantity: value,
+  });
+}
+
+menu.value.forEach((item) => {
+  item.quantity = 0;
+  orderList.forEach((order) => {
+    if (order.item_id == item.id) {
+      item.quantity = order.quantity;
+    }
+  });
+});
+
+// Computed
+const totalPrice = computed(() => {
+  let price = 0;
+  menu.value.forEach((item) => {
+    price = price + item.price * item.quantity;
+  });
+  return price;
+});
+</script>
+<template>
+  <!-- Hero -->
+  <section
+    class="pt-24 md:mt-0 h-screen flex flex-col-reverse justify-center text-center md:text-left md:flex-row md:justify-between md:items-center lg:px-48 md:px-12 px-4 bg-secondary"
+  >
+    <div v-if="orderData" class=" md:flex-1 md:mr-10">
+      <div
+        v-if="orderData?.done_by"
+        class="flex justify-center md:justify-start md:items-center"
+      >
+        <h1 class="xl:text-4xl text-2xl font-bold mb-7 text-left">
+          Toit on valmis! Tule ja naudi! :)
+        </h1>
+      </div>
+      <div v-else class="flex justify-center md:justify-start md:items-center">
+        <h1 class="xl:text-4xl text-2xl font-bold mb-7 text-left">
+          Toit on valmimas! :)
+        </h1>
+      </div>
+      <p class="font-normal">
+        <b>Sinu tellimus:</b>
+      </p>
+      <div v-for="item in menu" :key="item">
+        <p v-if="item.quantity > 0">
+          <b>{{ item.quantity }}x</b>
+          {{ item.name }}<br />
+        </p>
+      </div>
+    </div>
+    <div class="mx-auto justify-around lg:m-auto md:m-0 md:flex-1">
+      <div class="relative">
+        <img
+          src="/Highlight1.svg"
+          alt=""
+          class="absolute -top-14 -left-10 hidden lg:block"
+        />
+      </div>
+      <img
+        v-if="orderData?.done_by"
+        class="m-auto w-32 mb-10 md:w-full md:m-2 max-w-xs xl:max-w-lg 2xl:max-w-xl"
+        src="/pastaljano-pizza-pug.svg"
+        alt="pastaljano-pizza-pug"
+      />
+      <img
+        v-else
+        class="m-auto w-32 mb-10 md:w-full md:m-2 max-w-xs xl:max-w-lg 2xl:max-w-xl"
+        src="/pastaljano-lifestyle-pug.svg"
+        alt="pastaljano-lifestyle-pug"
+      />
+      <div class="relative">
+        <img
+          src="/Highlight2.svg"
+          alt=""
+          class="absolute -bottom-10 -right-6 hidden lg:block"
+        />
+      </div>
+    </div>
+  </section>
+</template>
